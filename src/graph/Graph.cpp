615c8e7769f89bcb8bbdf9d4834e0f529df12e53
Patricia do Carmo Nunes Oliveira <up201905427@fe.up.pt>
2021-05-17 13:42:38 +0100

    Add some methods to graph class

--- src/graph/Graph.cpp
+++ src/graph/Graph.cpp
@@ -1,1 +1,148 @@
 #include "Graph.h"
+
+#include <iostream>
+#include <unordered_set>
+
+#define VAN_SPEED 50
+
+std::vector<Vertex *> Graph::getVertexSet() const {
+    return this->_vertexSet;
+}
+
+Vertex* Graph::findVertex(const int &id) const {
+    std::unordered_map<int, Vertex*>::const_iterator it = _vertexHashMap.find(id);
+    return it == _vertexHashMap.end() ? nullptr : it->second;
+}
+
+bool Graph::addVertex(const int &id, int x, int y) {
+    if (findVertex(id) != nullptr) return false;
+
+    Vertex* newVertex = new Vertex(id, x, y);
+    _vertexSet.push_back(newVertex);
+    _vertexHashMap.insert(std::pair<int,Vertex*>(id, newVertex));
+    return true;
+}
+
+bool Graph::addEdge(int id, int origId, int destId) {
+    Vertex *orig = findVertex(origId);
+    Vertex *dest = findVertex(destId);
+    if(orig == nullptr || dest == nullptr) return false;
+
+    // Distance between to points in the Map
+    double dist = orig->getPosition().euclideanDistance(dest->getPosition()); // in meters
+
+    // TODO: pass speed to our time, according to distance
+    double time1 = dist * 3600 / (VAN_SPEED * 1000);
+    Time time = Time(0, 0);
+
+    // Suppose that all roads have 2 ways
+    orig->addEdge(id, orig, dest, time);
+    dest->addEdge(id+1, dest, orig, time);
+    return true;
+}
+
+int Graph::getNumVertex() const {
+    return (int) (this->_vertexSet).size();
+}
+
+void Graph::reset() {
+    for (auto & i : _vertexSet) delete i;
+    _vertexSet.clear();
+}
+
+Graph::~Graph() {
+    for (auto & i : _vertexSet) delete i;
+}
+
+void Graph::DFSVisit(Vertex* v) {
+    v->_visited = true;
+    for (const Edge &e : v->getAdj())
+        if (!e.getDest()->getVisited())
+            DFSVisit(e.getDest());
+}
+
+void Graph::DFSConnectivity(Vertex* start) {
+    for (Vertex* v : _vertexSet)
+        v->_visited = false;
+
+    DFSVisit(start);
+}
+
+void Graph::removeUnvisitedVertices() {
+    std::unordered_set<Vertex*, VertexHash, VertexHash> removed;
+
+    // Remove vertices
+    std::vector<Vertex *>::iterator v_it;
+    for (v_it = _vertexSet.begin(); v_it != _vertexSet.end(); v_it++) {
+        if (!((*v_it)->getVisited())) {
+            removed.insert(*v_it);
+            _vertexHashMap.erase((*v_it)->getId());
+            v_it = _vertexSet.erase(v_it);
+            v_it--;
+        }
+    }
+
+    // Remove edges
+    std::vector<Edge>::iterator e_it;
+    for (Vertex* v : _vertexSet) {
+        e_it = v->getAdj().begin();
+        for (; e_it != v->getAdj().end(); e_it++) {
+            if (removed.find((*e_it).getDest()) != removed.end()) {
+                e_it = v->getAdj().erase(e_it);
+                e_it--;
+            }
+        }
+    }
+}
+
+// single source algorithms
+Vertex* Graph::initSingleSource(const int &origin) {
+
+}
+
+bool Graph::relax(Vertex* v, Edge edge) {
+
+}
+
+void Graph::dijkstraShortestPath(const int &origin) {
+
+}
+
+void Graph::dijkstraShortestPath(const int &origin, const int &dest) {
+
+}
+
+std::vector<int> Graph::getPathVertices(const int origin, const int dest) {
+
+}
+
+std::vector<Edge> Graph::getPathEdges(const int origin, const int dest) {
+
+}
+
+std::vector<int> Graph::getPath(const int origin, const int dest) {
+
+}
+
+// A*
+void Graph::AStar(const int &origin, const int &dest) {
+
+}
+
+double Graph::heuristic(Vertex* current, Vertex * dest) {
+
+}
+
+Vertex* Graph::AInitSingleSource(const int &origin) {
+
+}
+
+std::vector<int> Graph::AGetPathVertices(const int origin, const int dest) {
+
+}
+
+std::vector<Edge> Graph::AGetPathEdges(const int origin, const int dest) {
+
+}
+
+
--- src/graph/Graph.h
+++ src/graph/Graph.h
@@ -1,4 +1,52 @@
 #ifndef SRC_GRAPH_H
 #define SRC_GRAPH_H
 
+#include "Vertex.h"
+#include "../util/MutablePriorityQueue.h"
+
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+
+#define INF std::numeric_limits<double>::max()
+
+class Graph {
+public:
+    std::vector<Vertex *> getVertexSet() const;
+    Vertex* findVertex(const int &id) const;
+    bool addVertex(const int &id, int x, int y);
+    bool addEdge(int id, int origId, int destId);
+    int getNumVertex() const;
+
+    // single source algorithms
+    Vertex * initSingleSource(const int &origin);
+    bool relax(Vertex *v, Edge edge);
+    void dijkstraShortestPath(const int &origin);
+    void dijkstraShortestPath(const int &origin, const int &dest);
+    std::vector<int> getPathVertices(const int origin, const int dest);
+    std::vector<Edge> getPathEdges(const int origin, const int dest);
+    std::vector<int> getPath(const int origin, const int dest);
+    // --
+
+    // A*
+    void AStar(const int &origin, const int &dest);
+    double heuristic(Vertex* current, Vertex* dest);
+    Vertex* AInitSingleSource(const int &origin);
+    std::vector<int> AGetPathVertices(const int origin, const int dest);
+    std::vector<Edge> AGetPathEdges(const int origin, const int dest);
+    // --
+
+    void reset();
+    ~Graph();
+
+    void DFSConnectivity(Vertex* start);
+    void removeUnvisitedVertices();
+
+private:
+    std::unordered_map<int, Vertex *> _vertexHashMap;
+    std::vector<Vertex *> _vertexSet;
+
+    void DFSVisit(Vertex* v);
+};
+
 #endif //SRC_GRAPH_H
